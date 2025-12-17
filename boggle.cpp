20f36224220d30a5327904547592d3b0cfdcffbd
MJ Deus <mdeus@usc.edu>
2025-12-16 17:08:39 -0800

    testing some basic boggle logic

--- boggle.cpp
+++ boggle.cpp
@@ -94,6 +94,30 @@ std::set<std::string> boggle(const std::set<std::string>& dict, const std::set<s
 bool boggleHelper(const std::set<std::string>& dict, const std::set<std::string>& prefix, const std::vector<std::vector<char> >& board, 
 								   std::string word, std::set<std::string>& result, unsigned int r, unsigned int c, int dr, int dc)
 {
-//add your solution here!
+	if(r >= board.size() || c >= board[0].size()){
+		return false;
+	}
 
+	word += board[r][c];
+
+	// current word is not a prefix, so we have to just kill this
+	if(prefix.find(word) == prefix.end()){
+		return false;
+	}
+
+	bool downChain = boggleHelper(dict, prefix, board, word, result, r+dr, c+dc, dr, dc);
+
+	// found a bigger word down the chain
+	if(downChain){
+		return true;
+	}
+
+	// biggest word is here
+	if(dict.find(word) != dict.end()){
+		result.insert(word);
+		return true;
+	}
+
+	// not a word at all
+	return false;
 }
